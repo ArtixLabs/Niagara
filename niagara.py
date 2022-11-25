4a465d7d3b0887edb3c6126cdcc3d779c68c5f6d
art <art@example.com>
2022-11-25 11:23:20 -0500

    Fix version

--- niagara.py
+++ niagara.py
@@ -1,12 +1,12 @@
 import argparse
 from src import deps
 parser = argparse.ArgumentParser()
-VERSION="0.0.3"
+VERSION="0.0.5"
 parser.add_argument('-c','--config')
-parser.add_argument('-v', '--version')
+parser.add_argument('-v', '--version', action="store_true")
 args = parser.parse_args()
 def run():
-    if args.version:
+    if args.version == True:
         print(VERSION)
     if args.config:
         x = deps.Packages()
