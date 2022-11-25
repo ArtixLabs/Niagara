1a1a2a2cbc63007d1e8d7b994edc0dcc4a75dc79
art <art@example.com>
2022-11-25 15:54:00 -0500

    Revert

--- niagara.py
+++ niagara.py
@@ -3,15 +3,15 @@ from src import deps
 parser = argparse.ArgumentParser()
 VERSION="0.0.9"
 parser.add_argument('-c','--config')
-parser.add_argument('-p', '--packages', action="store_true")
+parser.add_argument('-p', '--packages')
 parser.add_argument('-v', '--version', action="store_true")
 args = parser.parse_args()
 def run():
     if args.version == True:
         print(VERSION)
-    if args.packages == True:
+    if args.packages:
         x = deps.Packages()
-        x.dump_all_pkgs()
+        x.dump_all_pkgs(args.packages)
     if args.config:
         x = deps.Packages()
         x.install_pkgs(args.config)
