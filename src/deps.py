32acb8766549b43bc64e9a508b81438a7ebe02c2
art <art@example.com>
2022-11-25 15:33:09 -0500

    Add source cmds

--- src/deps.py
+++ src/deps.py
@@ -357,4 +357,23 @@ class Pkgs:
                 for package in _pkgs:
                     self.pkgs.append(UPackage(name=package))
         f.close()
+class Generic():
+    def __init__(self, file):
+        self.file = file
+
+    def operate(self):
+        with open(self.file) as f:
+            _data = f.read()
+            if "source" in json.loads(_data):
+                for cmd in json.loads(_data)["source"]:
+                    if "link" in cmd:
+                        link = cmd["link"]
+                        if "cmds" in cmd:
+                            cmds = cmd["cmds"]
+                            for x in cmds:
+                                if "cmd" in x:
+                                    if "dir" in x:
+                                        subprocess.call(x["cmd"].split(), cwd=x["dir"])
+                                    else:
+                                        subprocess.call(x["cmd"].split())
 
