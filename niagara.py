ee28441b74a34e8fb49f611942a00fa343cd271e
art <art@example.com>
2022-11-25 10:32:28 -0500

    Update

--- niagara.py
+++ niagara.py
@@ -1,11 +1,13 @@
 import argparse
 from src import deps
 parser = argparse.ArgumentParser()
-
+VERSION="0.0.2"
 parser.add_argument('-c','--config')
-
+parser.add_argument('-v', '--version')
 args = parser.parse_args()
 def run():
+    if args.version:
+        print(VERSION)
     if args.config:
         x = deps.Packages()
         x.install_pkgs(args.config)
